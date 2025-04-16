5d720b7056bd70ef7e9a43420a8e8693b7c253b3
Nunez Arturo <arturo.nunez@lam.fr>
2025-04-16 20:52:58 +0200

    modularising Raphas code

--- commitall.sh
+++ commitall.sh
@@ -1,3 +1,19 @@
-git add *
-git commit -m "one of many commits"
-git push -u origin main
+#!/bin/bash
+
+# Exit if no commit message is provided
+if [ -z "$1" ]; then
+  echo "Usage: ./commitall.sh \"Your commit message here\""
+  exit 1
+fi
+
+# Add all changes
+git add .
+
+# Commit with provided message
+git commit -m "$1"
+
+# Get current branch name
+branch=$(git symbolic-ref --short HEAD)
+
+# Push to current branch
+git push -u origin "$branch"
